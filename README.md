c29086638347052da2fb1073e0117c4d4231c27f
Decency Okobia <decencyokobia@Decencys-MacBook-Pro.local>
2025-01-30 03:22:35 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,74 @@
+# BMI Calculator
+
+A simple and interactive **BMI (Body Mass Index)** calculator built with **HTML**, **CSS**, and **JavaScript**. This tool allows users to input their weight and height to calculate their BMI and determine their body weight category (Underweight, healthy weight, Overweight, or Obese).
+
+---
+
+## Table of Contents
+
+- [BMI Calculator](#bmi-calculator)
+  - [Table of Contents](#table-of-contents)
+  - [Project Description](#project-description)
+  - [Features](#features)
+  - [Technologies Used](#technologies-used)
+  - [Installation](#installation)
+  - [Usage](#usage)
+
+---
+
+## Project Description
+
+This project is a **BMI Calculator** that helps users quickly calculate their Body Mass Index and understand where they fall within various health categories:
+
+- **Underweight** (BMI < 18.5)
+- **Normal weight** (BMI 18.5 - 24.9)
+- **Overweight** (BMI 25 - 29.9)
+- **Obesity** (BMI ≥ 30) etc...
+
+The calculator is designed to be simple, user-friendly, and responsive, making it accessible on both desktop and mobile devices.
+
+---
+
+## Features
+
+- **BMI Calculation**: Enter your weight and height, and the app calculates your BMI.
+- **Body Weight Category**: Displays your BMI and associated category.
+- **Responsive Design**: The app is fully responsive and works on both desktop and mobile devices.
+- **Clean UI**: Minimalistic, easy-to-understand interface.
+
+---
+
+## Technologies Used
+
+- **HTML**: For the basic structure of the calculator.
+- **CSS**: For styling and making the app responsive.
+- **JavaScript**: To handle BMI calculation and dynamic updates.
+
+---
+
+## Installation
+
+To run this project locally, follow these steps:
+
+1. **Clone the repository**:
+   ```bash
+   git clone https://github.com/decencyokobia/BMI-Calculator.git
+   ```
+
+2. **Navigate into the project directory**:
+   ```bash
+   cd bmi-calculator
+   ```
+
+3. **Open the `index.html` file** in your preferred web browser to start using the app.
+
+---
+
+## Usage
+
+1. Enter your weight in **kilograms (kg)** and height in **centimeters (cm)**.
+2. Click the **Calculate** button.
+3. Your **BMI** and corresponding weight category will be displayed.
+
+**Note**: Make sure to enter your height and weight values correctly for an accurate result.
+
---
+++ main.js
@@ -0,0 +1,87 @@
+const calculate = document.getElementById("calculate_btn");
+
+
+calculate.onclick = bmiCalculation;
+
+
+function bmiCalculation(){
+    const display = document.getElementById("bmi_display");
+    const weight = document.getElementById("weight_input").value;
+    const height = document.getElementById("height_input").value;
+    const message = document.getElementById("message");
+
+
+    if(!weight || !height){
+       alert(`complete all fields`);
+        return;
+    }
+
+    else {
+     
+    display.value = Math.round(weight / ((height / 100) * 2) * 10)/ 10;
+
+        if(display.value < 18.5){
+        message.textContent = `You are Underweight`;
+        display.className = "underweight";
+        message.className = "text";
+        }
+        else if (display.value <= 24.9){
+        message.textContent = `Healthy Weight! 👍🏽`;
+        display.className = "healthy_Weight";
+        message.className = "text";
+        }
+        else if (display.value <= 29.9){
+        message.textContent = `You are Overweight`;
+        display.className = "overweight";
+        message.className = "text";
+        }
+        else if (display.value <= 34.9){
+        message.textContent = `Obesity (Class 1)`;
+        display.className = "obese";
+        message.className = "text";
+        }
+        else if (display.value <= 39.9){
+        message.textContent = `Obesity (Class 2)`;
+        display.className = "obese";
+        message.className = "text";
+        }
+        else {
+        message.textContent = `Obesity (Class 3)`;
+        display.className = "obese";
+        message.className = "text";
+        }
+}
+}
+
+
+const maleButton = document.getElementById("male_btn");
+const femaleButton = document.getElementById("female_btn");
+
+maleButton.onclick = hideFemaleSex;
+femaleButton.onclick = hideMaleSex;
+
+
+function hideFemaleSex(){
+        femaleButton.className = `check_icon2`;
+    }
+function hideMaleSex(){
+        maleButton.className = `check_icon1`;
+    }
+
+
+
+const reset = document.getElementById("homeKey");
+
+reset.onclick = resetFields;
+
+function resetFields(){
+    document.getElementById("bmi_display").value = " ";
+    document.getElementById("weight_input").value = " ";
+    document.getElementById("height_input").value = " ";
+    document.getElementById("message").textContent = `BMI`;
+    document.getElementById("male_btn").className = `sex`;
+    document.getElementById("female_btn").className = `sex`;
+}
+
+
+
